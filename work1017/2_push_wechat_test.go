1331e3c0b658118a0652b401ba5e93ae301196cf
linyuana <52093255+linyuana@users.noreply.github.com>
2022-10-19 22:38:45 +0800

    22.10.17 homework

--- work1017/2_push_wechat_test.go
+++ work1017/2_push_wechat_test.go
@@ -53,7 +53,7 @@ func pushWeChat(title, desp string) (string, string, string) {
 	//从文件读取sendkey
 	f, err := os.Open("sendkey.txt")
 	if err != nil {
-    	panic(err)
+    		panic(err)
 	}
 	defer f.Close()
 	reader := bufio.NewReader(f)
